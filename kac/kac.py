8a32b48596308f23d5577b20081f82ee7066ab6d
Adam Walsh <adamtwalsh@gmail.com>
2020-01-09 19:06:34 -0600

    Remove end method, replace with click's Abort exception

--- kac/kac.py
+++ kac/kac.py
@@ -30,17 +30,13 @@ def bump(filename):
         choices=[v_num for v_num in c.available_new_versions.keys()]
     ).ask()
     if new_v_num is None:
-        end()
+        raise click.Abort
     # Confirm selected new version
     should_bump: bool = questionary.confirm(
         message=f'Bump Changelog to v{new_v_num}?'
     ).ask()
     # Bump or end
-    c.bump(c.available_new_versions[new_v_num]) if should_bump is True else end()
+    if not should_bump:
+        raise click.Abort
+    c.bump(c.available_new_versions[new_v_num])
     click.echo(f'Bumped to v{new_v_num}!')
-
-
-def end():
-    """Exit."""
-    print('Not bumping Changelog.')
-    exit()
