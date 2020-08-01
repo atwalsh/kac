10aa56d22c8d316b96ff36e8767ea9183607f2cc
Adam Walsh <adamtwalsh@gmail.com>
2020-08-01 17:31:48 -0400

    Update Footer docstring

--- kac/changelog/section/footer.py
+++ kac/changelog/section/footer.py
@@ -3,6 +3,7 @@ from .section import Section
 
 class Footer(Section):
     """
-    Represents the footer section of the Changelog, consisting of all text before the Body section.
+    Represents the footer section of the Changelog - consisting of all text before the Body section, usually URL
+    references to release tags.
     """
     _regex_pattern = r'(\[Unreleased\]:[\s\S]+)'
