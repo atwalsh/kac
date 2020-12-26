6802c4cfc93ac6576e358b5fd375418567d651ef
Adam Walsh <adamtwalsh@gmail.com>
2020-12-26 12:58:43 -0500

    Update arguments copy and bump commands

--- kac/kac.py
+++ kac/kac.py
@@ -1,66 +1,79 @@
 import os
 
 import click
+import pyperclip
 import questionary
 from jinja2 import Environment, PackageLoader
 from semver import parse_version_info as semver_parse
 
 from .changelog import Changelog
-from .changelog.util import parse_version_number
 
 
 @click.group()
 def cli():
+    """
+    A CLI tool for CHANGELOG files that follow the Keep-a-Changelog standard.
+    """
     pass
 
 
 @cli.command()
-@click.argument('filename', type=click.Path(exists=True), default=Changelog.default_file_name)
-@click.option('-v', '--version', 'version', default=Changelog.LATEST, required=True)
-def copy(filename, version):
-    """Copy a version's release text."""
-    c = Changelog(filename)
-    v = version
-    if v != Changelog.LATEST:
-        try:
-            v = parse_version_number(version)
-        except ValueError as e:
-            raise click.UsageError(str(e))
-    c.copy_release_text(v)
-
-
-@cli.command()
-@click.argument('filename', type=click.Path(exists=True), default=Changelog.default_file_name)
-def bump(filename):
-    """Bump a Changelog."""
-    c = Changelog(filename)
-    # Ask user to select new version
-    new_v_num: str = questionary.select(
-        message=f'Please select a new version (currently v{c.major}.{c.minor}.{c.patch})',
-        choices=[v_num for v_num in c.available_new_versions.keys()]
-    ).ask()
-    if new_v_num is None:
-        raise click.Abort
-    # Confirm selected new version
-    should_bump: bool = questionary.confirm(
-        message=f'Bump Changelog to v{new_v_num}?'
-    ).ask()
-    # Bump or end
-    if not should_bump:
-        raise click.Abort
-    c.bump(c.available_new_versions[new_v_num])
-    click.echo(f'Bumped to v{new_v_num}!')
+@click.option('-f', '--filename', 'filename', help='The filename of the CHANGELOG file to be created.',
+              default=Changelog.default_file_name,
+              type=click.Path(exists=True, dir_okay=False, writable=True, resolve_path=True), show_default=True)
+def copy(filename):
+    """Copy the latest release text."""
+    changelog = Changelog(filename)
+    pyperclip.copy(changelog.most_recent_release.changes_text)
+    click.echo(f'v{changelog.most_recent_version} release text copied to clipboard!')
 
 
 @cli.command()
 @click.option('-f', '--filename', 'filename', help='The filename of the CHANGELOG file to be created.',
-              default=Changelog.default_file_name_upper, type=click.Path(dir_okay=False, writable=True),
+              default=Changelog.default_file_name,
+              type=click.Path(exists=True, dir_okay=False, writable=True, resolve_path=True), show_default=True)
+@click.option('-p', '--pre-release', 'prerelease', help='The prerelease identifier token.', default='rc',
+              type=click.STRING, show_default=True)
+@click.option('-b', '--build', 'build', help='The build identifier token.', default='build', type=click.STRING,
               show_default=True)
-def init(filename: click.File):
+def bump(filename, build, prerelease):
+    """Bump a Changelog."""
+    changelog = Changelog(filename)
+    if not changelog.unreleased.has_changes:
+        click.echo('CHANGELOG has no unreleased changes.')
+        raise click.Abort
+
+    available_versions = changelog.get_next_versions(prerelease, build)
+
+    # Ask user to select new version
+    new_v_num: str = questionary.select(
+        message=f'Please select a new version (currently v{changelog.most_recent_version})',
+        choices=[v_num for v_num in available_versions.keys()]
+    ).ask()
+    if new_v_num is None:
+        raise click.Abort
+    new_version = available_versions[new_v_num]
+    # Confirm selected new version
+    should_bump: bool = questionary.confirm(
+        message=f'Bump Changelog to v{new_version}?'
+    ).ask()
+    # Bump or end
+    if not should_bump:
+        raise click.Abort
+
+    changelog.bump(new_version)
+
+    click.echo(f'Bumped to v{new_version}!')
+
+
+@cli.command()
+@click.option('-f', '--filename', 'filename', help='The filename of the CHANGELOG file to be created.',
+              default=Changelog.default_file_name, type=click.Path(dir_okay=False, writable=True, resolve_path=True),
+              show_default=True)
+def init(filename):
     """Create an empty CHANGELOG file."""
     # Check if the file already exists
-    full_path = os.path.realpath(click.format_filename(filename))
-    if os.path.isfile(full_path):
+    if os.path.isfile(filename):
         click.echo(f'The CHANGELOG file already exists!')
         raise click.Abort
 
@@ -97,9 +110,9 @@ def init(filename: click.File):
 
     # Write the new CHANGELOG file
     try:
-        with click.open_file(full_path, 'x') as f:  # `x` mode will fail to open if file exists
+        with click.open_file(filename, 'x') as f:  # `x` mode will fail to open if file exists
             f.write(new_file_text)
-            click.echo(f'Created CHANGELOG file at: {full_path}')
+            click.echo(f'Created CHANGELOG file at: {filename}')
     except FileExistsError:
         click.echo('The CHANGELOG file already exists!')
         raise click.Abort
