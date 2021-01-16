d4bec3bba86c6cca1a1166a26b665afbb938a5bf
Adam Walsh <adamtwalsh@gmail.com>
2021-01-16 17:14:18 -0500

    Remove old tests and util method

--- kac/changelog/util.py
+++ kac/changelog/util.py
@@ -1,23 +1,3 @@
-import re
-from typing import Tuple
-
-
-def parse_version_number(val: str) -> Tuple[int, int, int]:
-    """
-    Parse a version number. Supports vX.X.X and X.X.X formats.
-
-    :param val: The version number to parse.
-    :return: Tuple of ints representing the version.
-    """
-    _version_re_pattern: str = r'v?(\d+)\.(\d+)\.(\d+)'
-    pattern = re.compile(_version_re_pattern)
-    match = pattern.match(val)
-    if match is None:
-        raise ValueError(f'Invalid version number {val}')
-    groups = match.groups()
-    return int(groups[0]), int(groups[1]), int(groups[2])
-
-
 def rreplace(s: str, old: str, new: str, occurrence: int) -> str:
     """
     Reverse replace.
